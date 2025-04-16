5923600b6ffc0ae76ddf6b3effbdc10a3814ff6b
Your Name <farhamostafa657@gmail.com>
2025-04-16 10:06:40 +0400

    uploading image to supabase

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -12,14 +12,37 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabun-image/${imageName}`;
+
+  //create cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.log(error);
     throw new Error("cabins can not be created");
+  }
+
+  //upload image
+  // Use the JS library to download a file.
+
+  const { error: storageError } = await supabase.storage
+    .from("cabun-image")
+    .upload(imageName, newCabin.image);
+
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error(
+      "cabin image could not be uploaded and the cabin was not be created"
+    );
   }
 
   return data;
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://caarfghzonuxxzvbwcsv.supabase.co";
+export const supabaseUrl = "https://caarfghzonuxxzvbwcsv.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhYXJmZ2h6b251eHh6dmJ3Y3N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3MDE4ODUsImV4cCI6MjA2MDI3Nzg4NX0.MnFU_faU-Zh9WOGPxq3m6zTBFWd2Mv1Z7GkKBXhcMA8";
 const supabase = createClient(supabaseUrl, supabaseKey);
